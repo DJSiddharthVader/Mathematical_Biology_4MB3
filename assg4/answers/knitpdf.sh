f89373f94fadd0dd8afd277677b3aad32093e272
Sid Reed <sidreed34@gmail.com>
2019-03-04 02:43:16 -0500

    added 2a and 2b, should compile now by runnign ./knitpdf.sh

--- assg4/answers/knitpdf.sh
+++ assg4/answers/knitpdf.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #check valid file provided
 if [ $# -eq 0 ]; then
     base="main"
@@ -10,6 +9,7 @@ else
     #check file exists
     if [ ! -f "$file " ]; then
         echo "file $1 not found"
+        exit 1
     fi
     #check extension is .Rnw
     extension="$(basename $file | cut -f2 -d'.')"
@@ -17,16 +17,17 @@ else
         base="$(basename $file | cut -f1 -d'.')"
     else
         echo "invalid file extension ($extension), file must be .Rnw file"
+        exit 1
     fi
 fi
 #Compiling file
 if [[ $(which latexmk) ]]; then
     latexmk -c
-    R -q -e "library('knitr'); knitr::knit('$1')"
-    latexmk -pdf "$base"
-    latexmk -pdf  "$base"
+    R -q -e "library('knitr'); knitr::knit('$base.Rnw')"
+    latexmk -pdf "$base".tex
+    latexmk -pdf  "$base".tex
 else
-    R -q -e "library('knitr'); knitr::knit('$1')"
+    R -q -e "library('knitr'); knitr::knit('$base.Rnw')"
     pdflatex "$base".tex
     biber "$base"
     pdflatex "$base".tex
---
+++ assg4/answers/sections/gillespie.R
@@ -0,0 +1,80 @@
+#!/usr/bin/env Rscript
+library("deSolve")
+SI.Gillespie <- function(beta,N,I0,tmax){
+    #i > 0?
+    if (I0 == 0){
+        stop('initial infected must be > 0 for epidemic to progress')
+    }
+    #set vars
+    i = I0
+    incidences = c(i)
+    t = 0
+    times = c(0)
+    s = N - i
+    #main loop
+    while ( t < tmax ){
+        #time till next event
+        rnd <- runif(1,0,1)
+        dt = (1/beta)*(log((1/(1-rnd))))
+        t = t + dt
+        #only event is infection
+        news = s - (beta/N)*s*i
+        newi = i + (beta/N)*s*i
+        #update vars
+        s = news
+        i = newi
+        incidences = c(incidences,i)
+        times = c(times,t)
+    }
+    return(list(times,incidences))
+}
+SI.vector.field <- function(t,vars,params){
+    with(as.list(c(params, vars)), {
+        dx <- -Beta*x*y
+        dy <- Beta*x*y
+        vec.fld <- c(dx=dx,dy=dy)
+        return(list(vec.fld))
+    })
+}
+SI.Deterministic <- function(beta,N,I0,tmax,timepts=500){
+    #i > 0?
+    if (I0 == 0){
+        stop('initial infected must be > 0 for epidemic to progress')
+    }
+    #set vars
+    ic <- c(x=N - I0,y=I0)
+    times <- seq(0,tmax,by=tmax/timepts)
+    params <- c(Beta=beta/N)
+    #solve ode
+    incidences <- ode(ic,times,SI.vector.field,params)
+    return(list(times,incidences[,"y"]))
+}
+multipanel <-  function(realizations,beta,ns,I0,tmax){
+    par(mfrow = c(2,2))
+    for (popsize in ns){
+        #plot initial stochatic
+        result <- SI.Gillespie(beta,popsize,I0,tmax)
+        plot(result[[1]],
+             result[[2]],
+             col="blue",
+             type="l",
+             xlab="Time (t)",
+             ylab="Incidence (I(t))",
+             main=paste("N =",popsize))
+        #plot 30 stochastic realizations
+        for (i in seq(0,realizations-1)){
+            result <- SI.Gillespie(beta,popsize,I0,tmax)
+            lines(result[[1]],result[[2]],col="blue",type="l")
+        }
+        #deterministic solution
+        result <- SI.Deterministic(beta,popsize,I0,tmax)
+        lines(result[[1]],result[[2]],col="red",type="l")
+        #legend
+        legend("right",
+               legend=c("Stochastic","Deterministic"),
+               col=c("blue","red"),
+               box.lty=0,
+               lty=1:1,
+               cex=0.8)
+    }
+}
