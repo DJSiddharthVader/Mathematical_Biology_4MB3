b970d9570417e3ef244da6725dd10aa31ef7dd9d
Sid Reed <sidreed34@gmail.com>
2019-04-15 23:49:00 -0400

    minor typo, detail fixed

--- project/wordcount.sh
+++ project/wordcount.sh
@@ -2,6 +2,6 @@
 if [ $# -eq 0 ]; then
     echo "usage: bash wordcount.sh yourfile.tex"
 else
-    words="$(detex $1 | wc -w)"
+    words="$(detex $1  | grep -v "block" | grep -v "blank" | grep -v "draw" | grep -v "node" | grep -v "d[SIRW]" | wc -w)"
     echo "Document has $words words"
 fi
